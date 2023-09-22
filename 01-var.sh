205441177be4491b1370094ebd73fd215f682bd4
Arpana <muriki.arpana@gmail.com>
2023-09-22 15:48:20 +0530

    Create variables and access them

--- 01-var.sh
+++ 01-var.sh
@@ -6,3 +6,8 @@ b=apple
 c=true
 echo a = $a
 echo a in currency = ${a}USD
+
+Date=2021-12-08
+echo Good Morning,welcome, Todays Date is ${DATE}
+Date=$(date +%F)
+echo Good Morning, Welcome, todays date is ${DATE}

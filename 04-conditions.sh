ed9b75117f2265b7f8fac910cb6eb1b774c2f074
Arpana <muriki.arpana@gmail.com>
2023-09-24 18:20:52 +0530

    Conditions

--- 04-conditions.sh
+++ 04-conditions.sh
@@ -1,8 +1,11 @@
 read -p 'Enter your age:' age
+if [ -z "$age" ]; then
+  echo Input Missing
+  fi
 if [ "${age}" -lt 18 ]; then
    echo you are Minor
    elif [ "${age}" -gt 60 ]; then
-     echo you are Senior Citgen
+     echo you are Senior Citizen
    else
      echo you are Major
      fi
